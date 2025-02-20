2a349b7e525faf37908c7e24402ce6f645a70cb3
Julia Koda <julia.koda2001@gmail.com>
2025-02-20 18:42:05 +0200

    fixing logo issues

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -72,7 +72,7 @@ const config: Config = {
       title: 'My Site',
       logo: {
         alt: 'My Site Logo',
-        src: 'static\img\logo_romeo and juliet.png',
+        src: 'img/juliet.jpg',
       },
       items: [
         {
