f1ccafaa9ac4e3b76955ba787d9f6ba0ec8e16d9
Julia Koda <julia.koda2001@gmail.com>
2025-02-21 13:41:09 +0200

    fixed issues

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'Atlassian',
-  tagline: 'Let's unleash the potential of every team',
+  tagline: 'Lets unleash the potential of every team',
   favicon: 'img/atlassian logo.svg',
 
   // Set the production url of your site here
